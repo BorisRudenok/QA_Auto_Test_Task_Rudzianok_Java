eb701c24bdce19ba5143049fa453d6325be8439a
Barys Rudzianok <maadgorilla@gmail.com>
2021-12-17 17:11:48 +0300

    completed the task

--- QA_Auto_Test_Task_Rudzianok_Java/src/app/TestTask.java
+++ QA_Auto_Test_Task_Rudzianok_Java/src/app/TestTask.java
@@ -6,9 +6,9 @@ public class TestTask {
         TaskThree taskThree = new TaskThree();
         TaskOne taskOne = new TaskOne();
         TaskTwo taskTwo = new TaskTwo();
-//        taskOne.checkNumber(taskOne.inputNumber());
-//        taskTwo.checkName(taskTwo.inputName());
         int[] Array = {3, 9, 5, 12, 5, 6, 2, 7, 9, 55, 6, 24, 999};
+        taskOne.checkNumber(taskOne.inputNumber());
+        taskTwo.checkName(taskTwo.inputName());
         taskThree.checkArray(Array);
     }
 }
